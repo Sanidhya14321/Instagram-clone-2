599cc40a774f0655ffe0040fa5e1f3c8c5c0d631
Sanidhya14321 <sanidhya14321@gmail.com>
2024-12-30 22:48:02 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,26 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const app=express();
 
+app.get("/",(req,res)=>{
+    return res.status(200).json({
+        message:"I'm coming from backend",
+        success:true
+    })
+})
+
 //MIDDLEWARE
 app.use(express.json());
 app.use(cookieParser());
+app.use(urlencoded({extended:true}));
+const corsOptions={
+    origin:'http://localhost:5173',
+    credentials:true,
+}
 
+app.use(cors(corsOptions));
 
 const PORT=8000;
 
