7793847ae4ceb7e9173d7fb04dcac3118dd87371
Sanidhya14321 <sanidhya14321@gmail.com>
2025-01-06 22:18:08 +0530

    commit 10

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt  from  "jsonwebtoken"
 
 export const register = async (req, res) => {
   try {
@@ -48,7 +49,43 @@ export const login=async(req,res)=>{
                 success:false
             })
         }
+        const isPasswordMatch= await bcrypt.compare(password,user.password)
+        if(!isPasswordMatch){
+          return res.status(401).json({
+            message: "Something is missing, please check!",
+            success: false,
+          });
+        }
+
+        user={
+          _id:user._id,
+          username:user.username,
+          email:user.email,
+          profilePicture:user.profilePicture,
+          bio:user.bio,
+          followers:user.followers,
+          following:user.following,
+          posts:user.posts
+        }
+
+        const token= await jwt.sign({userId:user._id},process.env.SECRET_KEY,{expiresIn:'1d'})
+        return res.cookie('token',token,{httpOnly:true,sameSite:'strict',maxAge:1*24*60*60*100}).json({
+          message:`Welcome Back ${user.username}`,
+          success:true
+        })
+
     } catch (error) {
         console.log(error)
     }
+}
+
+export const logout=async(_,res)=>{
+  try {
+    return res.cookie("token","",{maxAge:0}).json({
+      message:"Logged Out Successfully",
+      success:true
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
