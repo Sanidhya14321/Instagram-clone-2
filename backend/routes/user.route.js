08af5e1bd3768e30aefee7a8930f5eaa19297366
Sanidhya14321 <sanidhya14321@gmail.com>
2025-02-07 03:02:24 +0530

    commit 26

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,19 +1,16 @@
 import express from "express";
+import { editProfile, followOrUnfollow, getProfile, getSuggestedUsers, login, logout, register } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import upload from "../middlewares/multer.js";
-import { addComment, addNewPost, bookmarkPost, deletePost, dislikePost, getAllPost, getCommentsOfPost, getUserPost, likePost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
-router.route("/addpost").post(isAuthenticated, upload.single('image'), addNewPost);
-router.route("/all").get(isAuthenticated,getAllPost);
-router.route("/userpost/all").get(isAuthenticated, getUserPost);
-router.route("/:id/like").get(isAuthenticated, likePost);
-router.route("/:id/dislike").get(isAuthenticated, dislikePost);
-router.route("/:id/comment").post(isAuthenticated, addComment); 
-router.route("/:id/comment/all").post(isAuthenticated, getCommentsOfPost);
-router.route("/delete/:id").delete(isAuthenticated, deletePost);
-router.route("/:id/bookmark").get(isAuthenticated, bookmarkPost);
+router.route('/register').post(register);
+router.route('/login').post(login);
+router.route('/logout').get(logout);
+router.route('/:id/profile').get(isAuthenticated, getProfile);
+router.route('/profile/edit').post(isAuthenticated, upload.single('profilePhoto'), editProfile);
+router.route('/suggested').get(isAuthenticated, getSuggestedUsers);
+router.route('/followorunfollow/:id').post(isAuthenticated, followOrUnfollow);
 
 export default router;
-
